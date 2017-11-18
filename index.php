09dac59cf4315f2d2026a25c533b675c17c74cb0
ThomasJones69 <tjones@ziggo.nl>
2017-11-18 15:52:47 +0100

    Inlog scherm klaar en layout

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel = "stylesheet" type = "text/css" href="layout.css"> 
         <title></title>
     </head>
     <body>
         <?php
         include 'connect.php';
-        
         ?>
+
+        <h2>Login SQL Weekpdracht inlog</h2>
+
+        <form action="/action_page.php">
+
+            <div class="container">
+                <label><b>Gebruiker</b></label>
+                <input type="text" placeholder="Uw gebruikersnaam" name="user" required>
+                <br>
+                <label><b>Wachtwoord</b></label>
+                <input type="password" placeholder="Voer Wachtwoord in" name="pass" required>
+                <br>
+                <button type="submit" width="100px">Inloggen</button>
+            </div>
+
+            <div class="container" style="background-color:#f1f1f1">
+                <button type="button" class="cancelbtn">Cancel</button>
+            </div>
+        </form>
+
     </body>
 </html>
--- te-lezen-file.php
+++ te-lezen-file.php
@@ -1,3 +1,8 @@
+<html>
+    <head>
+        <link rel = "stylesheet" type = "text/css" href="layout.css"> 
+    </head>
+</html>
 <?php
 
 echo "Als je dit kan lezen, Dan ben je ingelogt";
