c8dd6ba51202e3e396126dde0767407ee22b4f7b
ThomasJones69 <tjones@ziggo.nl>
2017-11-19 14:51:47 +0100

    Inloggen doet het niet. Hulptroepen ingeschakeld

--- connect.php
+++ connect.php
@@ -2,12 +2,33 @@
 
 function connectionDB() {
 
-    $hostname = 'localhost';            // the credentials of the connection
+    $hostname = 'localhost';            
     $databasenaam = 'user-accounts';
     $username = 'root';
     $password = '';
 
-    $conn = new mysqli($hostname, $username, $password, $databasenaam); // the instantiation of the mysqli object, on object TOTALLY specialised in DATABAS MANAGEMENT
-    return $conn;  // THAT object and connection is given back so that it can be catched at the call.
+    $conn = new mysqli($hostname, $username, $password, $databasenaam); 
+    if ($conn->connect_error) die($conn->connect_error);
+    return $conn;  
+}
+
+function inloggen(){
+    $naam = $_REQUEST['user'];
+    $wachtwoord = $_REQUEST['pass'];
+    echo "$naam";
+    echo "$wachtwoord";
+    
+    $encrypt = crypt($wachtwoord, $salt);
+    
+    $sql = "SELECT * FROM `user-accounts` WHERE `username` = '$naam' AND `password` = '$password'";
+    echo $sql;
+//        $result = mysqli_query($connectie, $sql);
+//        $result = mysqli_query($sql);
+        $result = $conn->query($sql);
+        if ($result->num_rows == 0) {
+            echo "Geen account met die gegevens!";
+        }else{
+            echo "u bent ingelogt";
+        }
 }
 ?>
--- index.php
+++ index.php
@@ -7,11 +7,12 @@
     <body>
         <?php
         include 'connect.php';
+        session_start();
         ?>
 
         <h2>Login SQL Weekpdracht inlog</h2>
 
-        <form action="/action_page.php">
+        <form method="POST">
 
             <div class="container">
                 <label><b>Gebruiker</b></label>
@@ -20,13 +21,36 @@
                 <label><b>Wachtwoord</b></label>
                 <input type="password" placeholder="Voer Wachtwoord in" name="pass" required>
                 <br>
-                <button type="submit" width="100px">Inloggen</button>
+                <button type="submit" width="75px" onclick="inloggen()">Inloggen</button>
             </div>
 
             <div class="container" style="background-color:#f1f1f1">
-                <button type="button" class="cancelbtn">Cancel</button>
+                <button type="button" class="annuleerknop">Annuleren</button>
             </div>
         </form>
+<?php
+//    $naam = $_REQUEST['user'];
+//    $wachtwoord = $_REQUEST['pass'];
+//    echo "$naam";
+//    echo "$wachtwoord";
+//    
+//    $encrypt = crypt($wachtwoord, $salt);
+//    
+//    $sql = "SELECT * FROM `user-accounts` WHERE `username` = '$naam'";
+////        $result = mysqli_query($connectie, $sql);
+////        $result = mysqli_query($sql);
+//        $result = $conn->query($sql);
+//        if ($result->num_rows == 0) {
+//            echo "Geen account met die gegevens!";
+//        }
+    
+//    if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+//    echo "CRYPT_BLOWFISH is enabled!";
+//}else {
+//echo "CRYPT_BLOWFISH is not available";
+//}
 
+
+?>
     </body>
 </html>
